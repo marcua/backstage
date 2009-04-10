b1f56021a9b498383c633ff2a7084fdeb1eb4ebc
Edward Benson <=>
2009-04-10 16:55:14 -0400

    Turned in to jQuery plugin. Fixed so it works. Have to send the template in JavaScript though. Not really a problem for research purposes but would be nice if this wasn't the case for real use. The basic problem is that the brower takes some liberty re-ordering text nodes amongst the HTML nodes when you send the template as HTML text, so control statements embedded inside a table, for example, might show up outside the table when you actually ask for the innerHTML of a parent container. Perhaps there is some way in the DOM to get the raw text, not processed HTML, that would fix this problem.

--- webclient/src/backstage.js
+++ webclient/src/backstage.js
@@ -1,8 +1,16 @@
+jQuery.fn.template = function(value) {
+    if (value === undefined) {
+        return this.data("backstage_template");        
+    }
+    else {
+        this.data("backstage_template", value);
+        return this;
+    }
+};
 
-function merge(template, json) {
-    template.innerHTML = "{# This is a comment and will be removed from the output.}\n\n{.section songs}\n  <h2>Songs in '{playlist-name}'</h2>\n\n  <table width=\"100%\">\n  {.repeated section @}\n    <tr>\n      <td><a href=\"{url-base|htmltag}{url|htmltag}\">Play</a>\n      <td><i>{title}</i></td>\n      <td>{artist}</td>\n    </tr>\n  {.end}\n  </table>\n{.or}\n  <p><em>(No page content matches)</em></p>\n{.end}\n";
-    var templText = template.innerHTML;
-    var templ = jsontemplate.Template(templText.toString());
-    var rendered = templ.expand(json);
-    template.innerHTML = rendered;
+jQuery.fn.render = function(value) {
+    var templateString = this.template();
+    var template = jsontemplate.Template(templateString);
+    var rendered = template.expand(value);
+    this.html(rendered);
 }
--- webclient/src/demo.js
+++ webclient/src/demo.js
@@ -1,6 +1,7 @@
 
 
 function demo1merge() {
+    var template = '{# This is a comment and will be removed from the output.}{.section songs}<h2>Songs in {playlist-name}</h2>      <table width="100%">      {.repeated section @}        <tr>          <td><a href="{url-base|htmltag}{url|htmltag}">Play</a>          <td><i>{title}</i></td>          <td>{artist}</td>        </tr>      {.end}      </table>    {.or}      <p><em>(No page content matches)</em></p>    {.end}';
     
     // Data
     var playlist = {
@@ -20,13 +21,11 @@ function demo1merge() {
       ]
     };
 
-    // Tempalte
-    
-    merge($('#template')[0], playlist);
+    // Tempalte    
+    var t = $('#playlist');
+    t.template(template);
+    t.render(playlist);
 }
 
 
-
-query1: SELECT name, pet_name FROM people, pets WHERE (join happens);
-query2: SELECT name, pet_name FROM people, pets WHERE (join happens);
 
