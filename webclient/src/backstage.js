dc6849aeab9ee3b7c188f79d47e2309d9e8669e2
Edward Benson <edward.benson@gmail.com>
2009-04-16 15:04:44 -0400

    Enabled remote template, data loading

--- webclient/src/backstage.js
+++ webclient/src/backstage.js
@@ -1,6 +1,11 @@
+jQuery.fn.templateRemote = function(link) {
+	var me = this;
+	$.get(link, function(data){$(me).template(data);});
+}
+
 jQuery.fn.template = function(value) {
-    if (value === undefined) {
-        return this.data("backstage_template");        
+	if (value === undefined) {
+    	return this.data("backstage_template");        
     }
     else {
         this.data("backstage_template", value);
@@ -8,9 +13,23 @@ jQuery.fn.template = function(value) {
     }
 };
 
+jQuery.fn.renderRemote = function(link) {
+	var me = this;
+	$.getJSON(link,function(data){$(me).render(data);});
+}
+
 jQuery.fn.render = function(value) {
     var templateString = this.template();
     var template = jsontemplate.Template(templateString);
     var rendered = template.expand(value);
     this.html(rendered);
+}
+
+function mergeTemplates() {
+	var template = '{# This is a comment and will be removed from the output.}{.section songs}<h2>Songs in {playlist-name}</h2>      <table width="100%">      {.repeated section @}        <tr>          <td><a href="{url-base|htmltag}{url|htmltag}">Play</a>          <td><i>{title}</i></td>          <td>{artist}</td>        </tr>      {.end}      </table>    {.or}      <p><em>(No page content matches)</em></p>    {.end}';
+	$("[datasource]").each(function(i) {
+		var template = $(this).innerHTML;
+		$(this).templateRemote($(this).attr('template'));
+		$(this).renderRemote($(this).attr('datasource'));
+	});	
 }
--- webclient/src/demo.js
+++ webclient/src/demo.js
@@ -1,31 +1,4 @@
 
 
-function demo1merge() {
-    var template = '{# This is a comment and will be removed from the output.}{.section songs}<h2>Songs in {playlist-name}</h2>      <table width="100%">      {.repeated section @}        <tr>          <td><a href="{url-base|htmltag}{url|htmltag}">Play</a>          <td><i>{title}</i></td>          <td>{artist}</td>        </tr>      {.end}      </table>    {.or}      <p><em>(No page content matches)</em></p>    {.end}';
-    
-    // Data
-    var playlist = {
-      "url-base": "http://example.com/music/", 
-      "playlist-name": "Epic Playlist", 
-      "songs": [
-        {
-          "url": "1.mp3", 
-          "artist": "Grayceon", 
-          "title": "Sounds Like Thunder"
-        }, 
-        {
-          "url": "2.mp3", 
-          "artist": "Thou", 
-          "title": "Their Hooves Carve Craters in the Earth"
-        }
-      ]
-    };
-
-    // Tempalte    
-    var t = $('#playlist');
-    t.template(template);
-    t.render(playlist);
-}
-
 
 
