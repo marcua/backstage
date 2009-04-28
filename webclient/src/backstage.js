3fa151917592b8a126ea9cddd43d581f1af55888
Edward Benson <=>
2009-04-27 20:41:16 -0400

    Some updates to wb client. Timing things, etc

--- webclient/src/backstage.js
+++ webclient/src/backstage.js
@@ -13,21 +13,80 @@ jQuery.fn.template = function(value) {
     }
 };
 
-jQuery.fn.renderRemote = function(link) {
+jQuery.fn.renderLocal = function(datasource, dataset, query) {
+	// First download any data source we don't already have
+	var datasources = dataset.split(",");
+	for (var i=0; i<datasources.length; i++) {
+		var haveDatasource = false;
+		if (! haveDatasource) {
+			console.time("Downloading " + datasources[i]);
+			$.get(datasources[i],
+				function(data){
+					console.timeEnd("Downloading " + datasources[i]);
+					console.time("Bulkloading " + datasources[i]);
+					alert(data);
+					console.timeEnd("Bulkloading " + datasources[i]);
+				}
+			);					
+		}
+	}
+}
+
+jQuery.fn.renderRemote = function(datasource, dataset, query) {
 	var me = this;
-	$.getJSON(link,function(data){$(me).render(data);});
+	console.time("Querying Server");
+	if ((dataset === undefined) && (query === undefined)) {
+		// Render with just the datasource
+		$.getJSON(
+			datasource,
+			function(data){
+				console.timeEnd("Querying Server");
+				$(me).render(data);
+			}
+		);		
+	}
+	else if (query === undefined) {
+		// Render with datasource and dataset
+		$.getJSON(
+			datasource,
+			{
+				"uri":dataset
+			},
+			function(data){
+				console.timeEnd("Querying Server");
+				$(me).render(data);
+			}
+		);		
+	}
+	else {
+		// Render with datasource, dataset, and query
+		$.getJSON(
+			datasource,
+			{
+				"uri":dataset,
+				"query":query
+			},
+			function(data){
+				console.timeEnd("Querying Server");
+				$(me).render(data);
+			}
+		);		
+	}
 }
 
 jQuery.fn.render = function(value) {
+	console.time("Rendering Template");
     var templateString = this.template();
     var template = jsontemplate.Template(templateString);
+	console.log("Template Size: " + templateString.length);
     var rendered = template.expand(value);
+	console.log("Rendered Size: " + rendered.length);
     this.html(rendered);
+	console.timeEnd("Rendering Template");
 }
-
 function mergeTemplates() {
 	$("[datasource]").each(function(i) {
 		$(this).templateRemote($(this).attr('template'));
-		$(this).renderRemote($(this).attr('datasource'));
+		$(this).renderLocal($(this).attr('datasource'), $(this).attr('dataset'), $(this).attr('query'));
 	});	
 }
--- webclient/src/imdb_test.js
+++ webclient/src/imdb_test.js
@@ -1,7 +1,5 @@
 function imdbTest() {
-	var template = '{# This is a comment and will be removed from the output.}{.section songs}<h2>Songs in {playlist-name}</h2>      <table width="100%">      {.repeated section @}        <tr>          <td><a href="{url-base|htmltag}{url|htmltag}">Play</a>          <td><i>{title}</i></td>          <td>{artist}</td>        </tr>      {.end}      </table>    {.or}      <p><em>(No page content matches)</em></p>    {.end}';
 	$("[datasource]").each(function(i) {
-		var template = $(this).innerHTML;
 		$(this).templateRemote($(this).attr('template'));
 		$(this).renderRemote($(this).attr('datasource'));
 	});	
